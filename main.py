70bd6c690ae9214998d326515da745dedff158cf
Shammah Anucha <shammah.anucha@yahoo.com>
2022-06-17 11:52:41 +0200

    transparent

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ if st.button("Make Prediction"):
         x=full_set.Prediction.value_counts().index,
         y=full_set.Prediction.value_counts(),
         data=full_set,
-        color="#FF000000",
+        color="#FF 000000",
     )
     plt.title("Predicted Final Results", fontsize=20)
     ax.set_facecolor("None")
